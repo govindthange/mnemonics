f387b230618bc689dac46c4a9a3f75a292a1e2dd
Govind Thange <govindt@pune.winsoftech.com>
2020-05-15 19:58:10 +0530

    Identify object, color, attrib, theme, time etc

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import { interval, fromEvent } from "rxjs";
 import { map, filter, scan } from "rxjs/operators";
 
-let i$ = fromEvent(document, "keydown")
-        .pipe(filter((evt: any) => evt.keyCode >= 48 && evt.keyCode <= 57 ))
-        .pipe(map((evt: any) => evt.key))
-        .pipe(scan((str, v) => {
-            str = v + str;
-            return str;
-        }, ""));
+const node = document.querySelector('input[type=number]');
+const input$ = fromEvent(node, 'input')
+                .pipe(map((evt: any) => evt.target.value));
 
-i$.subscribe((d: string) => console.log(d));
+input$.subscribe({
+    next: v => {
+        console.log(`Memory mapping ${v}!`)
+        if (v > 0) {
+            let obj = {
+                object: v%100,
+                color: ((v - v % 100) / 100) % 10,
+                attribute: ((v - v % 1000) / 1000) % 10,
+                theme: ((v - v % 10000) / 10000) % 10,
+                time: ((v - v % 100000) / 100000) % 10,
+                characterType: ((v - v % 1000000) / 1000000) % 10,
+                character: ((v - v % 10000000) / 10000000) % 10,
+            };
+            console.log(obj);
+        }
+
+        },
+        error: err => console.log(`Oops... ${err}`),
+            complete: () => console.log(`Complete!`),
+});
