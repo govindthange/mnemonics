39794e5a6387c24493396afbf75d8944201a3dac
Govind Thange <govindt@pune.winsoftech.com>
2020-05-17 10:31:14 +0530

    Show calamity alert inside an orange triangle

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { interval, fromEvent } from "rxjs";
 import { map, filter, scan } from "rxjs/operators";
 
-const propImages = ["00-saw.png", "01-seed.png", "02-snow.png", "03-sumo.png", "04-sierra.png", "05-soil.png", "06-sewage.png", "07-sack.png", "08-sofa.png", "09-soap.png", "10-tissue.png", "11-toad.png", "12-tin.png", "13-dome.png", "14-tyre.png", "15-doll.png", "16-dish.png", "17-deck.png", "18-tv.png", "19-tap.png", "20-news.png", "21-net.png", "22-nanny.png", "23-NEEM.png", "24-sonar.png", "25-nail.png", "26-snowshoe.png", "27-nuke.png", "28-knife.png", "29-nib.png", "30-maze.png", "31-mat.png", "32-mine.png", "33-mummy.png", "34-hammer.png", "35-mail.png", "36-match.png", "37-mug.png", "38-movie.png", "39-mop.png", "40-rice.png", "41-rod.png", "42-rain.png", "43-ram.png", "44-rar.png", "45-rail.png", "no-image.png", "47-rake.png", "48-roof.png", "49-rope.png", "50-lace.png", "51-led.png", "52-lane.png", "no-image.png", "54-lorry.png", "55-lily.png", "56-leash.png", "57-lock.png", "58-leaf.png", "59-lab.png", "60-juice.png", "61-jet.png", "62-genie.png", "63-gem.png", "64-jar.png", "65-jail.png", "66-judge.png", "67-jack.png", "68-chef.png", "69-jeep.png", "70-gas.png", "71-kite.png", "72-gun.png", "73-cam.png", "74-car.png", "75-coil.png", "76-cash.png", "77-cake.png", "78-cuff.png", "79-cup.png", "80-vace.png", "81-video.png", "82-fan.png", "83-foam.png", "84-fire.png", "85-foil.png", "86-fish.png", "87-fog.png", "88-fifa.png", "89-vip.png", "90-bus.png", "91-bed.png", "92-bin.png", "93-palm.png", "94-bar.png", "95-bell.png", "96-pouch.png", "97-bike.png", "98-buffet.png", "99-pipe.png"];
-const conditionImages = ["0-smoked.jpg", "1-tarred.jpg", "2-nano-sized.png", "3-muddied.png", "4-rusted.png", "5-labelled.png", "6-chopped.png", "7-greased.png", "8-firedup.png", "9-pores.png"];
+// https://resizeimage.net/
+const propImages: string[] = ["00-saw.png", "01-seed.png", "02-snow.png", "03-sumo.png", "04-sierra.png", "05-soil.png", "06-sewage.png", "07-sack.png", "08-sofa.png", "09-soap.png", "10-tissue.png", "11-toad.png", "12-tin.png", "13-dome.png", "14-tyre.png", "15-doll.png", "16-dish.png", "17-deck.png", "18-tv.png", "19-tap.png", "20-news.png", "21-net.png", "22-nanny.png", "23-NEEM.png", "24-sonar.png", "25-nail.png", "26-snowshoe.png", "27-nuke.png", "28-knife.png", "29-nib.png", "30-maze.png", "31-mat.png", "32-mine.png", "33-mummy.png", "34-hammer.png", "35-mail.png", "36-match.png", "37-mug.png", "38-movie.png", "39-mop.png", "40-rice.png", "41-rod.png", "42-rain.png", "43-ram.png", "44-rar.png", "45-rail.png", "no-image.png", "47-rake.png", "48-roof.png", "49-rope.png", "50-lace.png", "51-led.png", "52-lane.png", "no-image.png", "54-lorry.png", "55-lily.png", "56-leash.png", "57-lock.png", "58-leaf.png", "59-lab.png", "60-juice.png", "61-jet.png", "62-genie.png", "63-gem.png", "64-jar.png", "65-jail.png", "66-judge.png", "67-jack.png", "68-chef.png", "69-jeep.png", "70-gas.png", "71-kite.png", "72-gun.png", "73-cam.png", "74-car.png", "75-coil.png", "76-cash.png", "77-cake.png", "78-cuff.png", "79-cup.png", "80-vace.png", "81-video.png", "82-fan.png", "83-foam.png", "84-fire.png", "85-foil.png", "86-fish.png", "87-fog.png", "88-fifa.png", "89-vip.png", "90-bus.png", "91-bed.png", "92-bin.png", "93-palm.png", "94-bar.png", "95-bell.png", "96-pouch.png", "97-bike.png", "98-buffet.png", "99-pipe.png"];
+const conditionImages: string[] = ["0-smoked.jpg", "1-tarred.jpg", "2-nano-sized.png", "3-muddied.png", "4-rusted.png", "5-labelled.png", "6-chopped.png", "7-greased.png", "8-firedup.png", "9-pores.png"];
 
 // https://www.spycolor.com/color-index,g
-const colorCodes = ["#c0c0c0", "#00f5ff", "#000080", "maroon", "red", "#ccff00", "#36454f", "#d4af37", "white", "black"];
+const colorCodes: string[] = ["#c0c0c0", "#00f5ff", "#000080", "maroon", "red", "#ccff00", "#36454f", "#d4af37", "white", "black"];
 
+const calamityImages: string[] = ["0-tsunami.png", "1-draught.png", "2-nuclear-war.png", "3-mob-lynching.png", "4-earthquake.png", "5-landslide.png", "6-jungle-fire.png", "7-comet.png", "8-volcano.png", "9-bomb.png"];
 
 /*
 * 1. Setup the PROP to map 4 digits
@@ -23,23 +25,24 @@ const COLOR_PLACE = 100;
 // Picture the site and time of the scene (3rd 2 digits)
 const SITE_PLACE = 100000;
 const TIME_PLACE = 10000;
+const CALAMITY_PLACE = 1000000;
 
 /*
 * 3. Setup ACTOR and its makeup to map next 4 digits
 */
 // Picture the actor (Next 2 digits)
-const ROLE_PLACE = 10000000;
-const CHARACTER_PLACE = 1000000;
+const ROLE_PLACE = 100000000;
+const CHARACTER_PLACE = 10000000;
 // Picture the actor's injury (Next 2 digits)
-const ORGAN_PLACE = 1000000000;
-const INJURY_PLACE = 100000000;
+const ORGAN_PLACE = 10000000000;
+const INJURY_PLACE = 1000000000;
 
 /*
 * 4. Setup the LOCATION to map next 2 digits
 */
 // Picture the memory palace and its stations (Next 2 digits)
-const LOCI_PLACE = 100000000000;
-const STATION_PLACE = 10000000000;
+const LOCI_PLACE = 1000000000000;
+const STATION_PLACE = 100000000000;
 
 /*
 * 5. Setup the PLANET to map next 2 digits
@@ -60,7 +63,8 @@ let numberToScene = (n: number) => {
         },
         theme: {
             site: getPegIndex(n, SITE_PLACE, 1),
-            time: getPegIndex(n, TIME_PLACE, 1)
+            time: getPegIndex(n, TIME_PLACE, 1),
+            calamity: getPegIndex(n, CALAMITY_PLACE, 1)
         },
         actor: {
             role: getPegIndex(n, ROLE_PLACE, 1),
@@ -89,6 +93,9 @@ let renderScene = (scene: any) => {
     if (condition) {
         condition.src = "./images/object-conditions/" + conditionImages[scene.prop.condition];
     }
+
+    let calamity: any = document.getElementById("calamity");
+    if (calamity) calamity.src = "./images/calamities/" + calamityImages[scene.theme.calamity];
 }
 
 const node = document.querySelector('input[type=number]');
