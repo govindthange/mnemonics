2ea1d4665205492359345ab5cf606c9b2d64073b
Govind Thange <govindt@pune.winsoftech.com>
2020-05-24 09:03:14 +0530

    Tag text to indicate the exact number being pegged

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import { map, filter, scan } from "rxjs/operators";
 
 // https://resizeimage.net/
 const propImages: string[] = ["00-saw.png", "01-seed.png", "02-snow.png", "03-sumo.png", "04-sierra.png", "05-soil.png", "06-sewage.png", "07-sack.png", "08-sofa.png", "09-soap.png", "10-tissue.png", "11-toad.png", "12-tin.png", "13-dome.png", "14-tyre.png", "15-doll.png", "16-dish.png", "17-deck.png", "18-tv.png", "19-tap.png", "20-news.png", "21-net.png", "22-nanny.png", "23-NEEM.png", "24-sonar.png", "25-nail.png", "26-snowshoe.png", "27-nuke.png", "28-knife.png", "29-nib.png", "30-maze.png", "31-mat.png", "32-mine.png", "33-mummy.png", "34-hammer.png", "35-mail.png", "36-match.png", "37-mug.png", "38-movie.png", "39-mop.png", "40-rice.png", "41-rod.png", "42-rain.png", "43-ram.png", "44-rar.png", "45-rail.png", "no-image.png", "47-rake.png", "48-roof.png", "49-rope.png", "50-lace.png", "51-led.png", "52-lane.png", "no-image.png", "54-lorry.png", "55-lily.png", "56-leash.png", "57-lock.png", "58-leaf.png", "59-lab.png", "60-juice.png", "61-jet.png", "62-genie.png", "63-gem.png", "64-jar.png", "65-jail.png", "66-judge.png", "67-jack.png", "68-chef.png", "69-jeep.png", "70-gas.png", "71-kite.png", "72-gun.png", "73-cam.png", "74-car.png", "75-coil.png", "76-cash.png", "77-cake.png", "78-cuff.png", "79-cup.png", "80-vace.png", "81-video.png", "82-fan.png", "83-foam.png", "84-fire.png", "85-foil.png", "86-fish.png", "87-fog.png", "88-fifa.png", "89-vip.png", "90-bus.png", "91-bed.png", "92-bin.png", "93-palm.png", "94-bar.png", "95-bell.png", "96-pouch.png", "97-bike.png", "98-buffet.png", "99-pipe.png"];
-const propLabels: string[] = ["A SAW","A SEED","A SNOWMAN","A SUMO","A SIERRA frame","A SOIL","A SEWAGE pipe","A SACK","A SOFA","A SOAP","A TISSUE","A TOAD","A TIN","A DOME","A TYRE","A DOLL","A DISH","A DECK","A TV","A TAP","A NEWS","A NET","A NANNY","A NEEM","A SONAR sensor","A NAIL","A SNOWSHOE","A NUKE","A KNIFE","A NIB","A MAZE","A MAT","A MINE","A MUMMY","A HAMMER","A MAIL","A MATCH","A MUG","A MOVIE","A MOP","A RICE","A ROD","A RAIN","A RAM","A RAR","A RAIL", "A RIDGE", "A RAKE","A ROOF","A ROPE","A LACE","A LED","A LANE", "A LOOM", "A LORRY","A LILY","A LEASH","A LOCK","A LEAF","A LAB","A JUICE","A JET","A GENIE","A GEM","A JAR","A JAIL","A JUDGE","A JACK","A CHEF","A JEEP","A GAS","A KITE","A GUN","A CAM","A CAR","A COIL","A CASH","A CAKE","A CUFF","A CUP","A VACE","A VIDEO","A FAN","A FOAM","A FIRE","A FOIL","A FISH","A FOGG deodorant spray","A FIFA","A VIP pass","A BUS","A BED","A BIN","A PALM","A BAR","A BELL","A POUCH","A BIKE","A BUFFET","A PIPE"];
+const propLabels: string[] = ["SAW","SEED","SNOWMAN","SUMO","SIERRframe","SOIL","SEWAGE pipe","SACK","SOFA","SOAP","TISSUE","TOAD","TIN","DOME","TYRE","DOLL","DISH","DECK","TV","TAP","NEWS","NET","NANNY","NEEM","SONAR sensor","NAIL","SNOWSHOE","NUKE","KNIFE","NIB","MAZE","MAT","MINE","MUMMY","HAMMER","MAIL","MATCH","MUG","MOVIE","MOP","RICE","ROD","RAIN","RAM","RAR","RAIL", "RIDGE", "RAKE","ROOF","ROPE","LACE","LED","LANE", "LOOM", "LORRY","LILY","LEASH","LOCK","LEAF","LAB","JUICE","JET","GENIE","GEM","JAR","JAIL","JUDGE","JACK","CHEF","JEEP","GAS","KITE","GUN","CAM","CAR","COIL","CASH","CAKE","CUFF","CUP","VACE","VIDEO","FAN","FOAM","FIRE","FOIL","FISH","FOGG deodorant spray","FIFA","VIP pass","BUS","BED","BIN","PALM","BAR","BELL","POUCH","BIKE","BUFFET","PIPE"];
 
 const conditionImages: string[] = ["0-scratched.png", "1-tarred.png", "2-nailed.png", "3-muddied.png", "4-rusted.png", "5-labelled.png", "6-chopped.png", "7-greased.png", "8-firedup.png", "9-papered.png"];
-const conditionLabels: string[] = [", with a SCRATCHED surface revealing its inside ", ", covered with TAR in ", ", sealed using NAILS in ", ", in a MUDDIED ", ", RUSTED in ", ", LABELLED in ", ", CHOPPED to reveal its inside ", ", GREASED in ", ", emitting FLAMES in ", ", wrapped with PAPER in "];
+const conditionLabels: string[] = [", with a SCRATCHED surface revealing its inside ", ", covered with TAR in ", ", sealed using NAILS in ", ", in a MUDDIED ", ", RUSTED in ", ", LABELLED in ", ", CHOPPED to reveal its inside ", ", GREASED in ", ", emitting FLAMES in ", ", all PAPERED in "];
 
 // https://www.spycolor.com/color-index,g
 const colorCodes: string[] = ["#f0f0f1", "#00f5ff", "#000080", "magenta", "red", "#ccff00", "#4f0013", "#ffd700", "white", "black"];
-const colorLabels: string[] = ["SILVER color, somewhere ", "TURQUOISE color, somewhere ", "NAVY BLUE color, somewhere ", "MAROON color, somewhere ", "RED color, somewhere ", "LEMON color, somewhere ", "CHOCOLATY color, somewhere ", "GOLD color, somewhere ", "WHITE color, somewhere ", "BLACK color, somewhere "];
+const colorLabels: string[] = ["SILVER color, somewhere ", "TURQUOISE color, somewhere ", "NAVY-BLUE color, somewhere ", "MAROON color, somewhere ", "RED color, somewhere ", "LEMON color, somewhere ", "CHOCOLATY color, somewhere ", "GOLD color, somewhere ", "WHITE color, somewhere ", "BLACK color, somewhere "];
 
+const locationImages: string[] = ["0-city.png", "1-desert.png", "2-north-pole.png", "3-mountain.png", "4-river.png", "5-lake.png", "6-jungle.png", "7-coast.png", "8-valley.png", "9-party.png"];
 const locationLabels: string[] = ["within CITY", "within a DESERT", "at the NORTH POLE", "within MOUNTAINS", "along side the RIVER", "by the LAKE", "in the JUNGLE", "at the COAST", "in the VALLEY", "at the PARTY DESTINATION"];
 const weatherLabels: string[] = [" while SNOWFALL"," in DUSTY weather"," in NORMAL weather"," in MOIST and humid weather"," under RAIN"," in heavy thunder LIGTNING"," in CHILLING weather"," under CLEAR SKY"," in FOGGY weather conditions"," POLLUTED by heavy traffic"];
 const timeColorCodes: string[] = ["black", "#191D30", "#93ABB5", "#87ceeb", "#d7e8fd", "#D4FFF7", "#FEFFD4", "#d7e8fd", "#455270", "#2C3342"];
@@ -113,7 +114,9 @@ let numberToScene = (n: number) => {
         site: {
             location: {
                 n: siteLocationIdx,
-                label: locationLabels[siteLocationIdx]
+                label: locationLabels[siteLocationIdx],
+                image: locationImages[siteLocationIdx],
+                imagePath: "./images/sites/clip-art/" + locationImages[siteLocationIdx]
             },
             weather: {
                 n: siteWeatherIdx,
@@ -165,22 +168,24 @@ let numberToScene = (n: number) => {
     };
 }
 
+let tagText = (obj: any) => {
+    let text: string = obj.label.replace(/(\b[A-Z][A-Z]+|\b[A-Z]\b)/g, ` <span class="keyword">$&<span class="keyword-tag">${obj.n}</span></span> `);
+    return text;
+}
+
 let renderLabels= (scene: any, labelId: string) => {
     console.log("Rendering label for Scene %o", scene);
 
-    let text = scene.prop.object.label
-                + scene.prop.condition.label
-                + scene.prop.color.label
-                + scene.site.location.label
-                + scene.site.weather.label
-                + scene.site.time.label
-                + scene.site.sound.label;
-    
-    text = text.replace(/(\b[A-Z][A-Z]+|\b[A-Z]\b)/g, ' <span class="keyword">$&</span> ');
-    text = text.replace('<span class="keyword">A</span>', "Picture a ");
+    let text = tagText(scene.prop.object)
+                + tagText(scene.prop.condition)
+                + tagText(scene.prop.color)
+                + tagText(scene.site.location)
+                + tagText(scene.site.weather)
+                + tagText(scene.site.time)
+                + tagText(scene.site.sound);
     
     let label: any = document.getElementById(labelId);
-    if (label) label.innerHTML = text;
+    if (label) label.innerHTML = "Picture a " + text;
 }
 
 let renderScene = (scene: any) => {
@@ -200,6 +205,9 @@ let renderScene = (scene: any) => {
     let siteContainer: any = document.getElementById("siteContainer");
     if (siteContainer) siteContainer.style.backgroundColor = scene.site.time.color;
 
+    let location: any = document.getElementById("location");
+    if (location) location.src = scene.site.location.imagePath;
+
     let time: any = document.getElementById("time");
     if (time) time.src = scene.site.time.imagePath;
 
@@ -217,11 +225,18 @@ input$.subscribe({
         let scene = numberToScene(n);
         renderLabels(scene, "label");
         renderScene(scene);
-        /*
-        let reverseN = parseInt(n.toString().split('').reverse().join(''));
+        
+        let arr = n.toString().split('').reverse();
+        if (arr.length >= 2) {
+            let temp = arr[arr.length-1];
+            arr[arr.length-1] = arr[arr.length-2];
+            arr[arr.length-2] = temp;
+        }
+
+        let reverseN = parseInt(arr.join(''));
         let reverseScene = numberToScene(reverseN);
         renderLabels(reverseScene, "reverseLabel");
-        */
+        
     },
     error: err => console.log(`Oops... ${err}`),
     complete: () => console.log(`Complete!`),
