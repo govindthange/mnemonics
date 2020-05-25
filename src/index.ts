f714e904b6c777da1c3127e2d15a6d8153512a57
Govind Thange <gthange@yahoo.com>
2020-05-25 17:34:38 +0530

    Show real life images in separate frame

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,16 @@ import { map, filter, scan } from "rxjs/operators";
 const propImages: string[] = ["00-saw.png", "01-seed.png", "02-snow.png", "03-sumo.png", "04-sierra.png", "05-soil.png", "06-sewage.png", "07-sack.png", "08-sofa.png", "09-soap.png", "10-tissue.png", "11-toad.png", "12-tin.png", "13-dome.png", "14-tyre.png", "15-doll.png", "16-dish.png", "17-deck.png", "18-tv.png", "19-tap.png", "20-news.png", "21-net.png", "22-nanny.png", "23-NEEM.png", "24-sonar.png", "25-nail.png", "26-snowshoe.png", "27-nuke.png", "28-knife.png", "29-nib.png", "30-maze.png", "31-mat.png", "32-mine.png", "33-mummy.png", "34-hammer.png", "35-mail.png", "36-match.png", "37-mug.png", "38-movie.png", "39-mop.png", "40-rice.png", "41-rod.png", "42-rain.png", "43-ram.png", "44-rar.png", "45-rail.png", "no-image.png", "47-rake.png", "48-roof.png", "49-rope.png", "50-lace.png", "51-led.png", "52-lane.png", "no-image.png", "54-lorry.png", "55-lily.png", "56-leash.png", "57-lock.png", "58-leaf.png", "59-lab.png", "60-juice.png", "61-jet.png", "62-genie.png", "63-gem.png", "64-jar.png", "65-jail.png", "66-judge.png", "67-jack.png", "68-chef.png", "69-jeep.png", "70-gas.png", "71-kite.png", "72-gun.png", "73-cam.png", "74-car.png", "75-coil.png", "76-cash.png", "77-cake.png", "78-cuff.png", "79-cup.png", "80-vace.png", "81-video.png", "82-fan.png", "83-foam.png", "84-fire.png", "85-foil.png", "86-fish.png", "87-fog.png", "88-fifa.png", "89-vip.png", "90-bus.png", "91-bed.png", "92-bin.png", "93-palm.png", "94-bar.png", "95-bell.png", "96-pouch.png", "97-bike.png", "98-buffet.png", "99-pipe.png"];
 const propLabels: string[] = ["SAW","SEED","SNOWMAN","SUMO","SIERRframe","SOIL","SEWAGE pipe","SACK","SOFA","SOAP","TISSUE","TOAD","TIN","DOME","TYRE","DOLL","DISH","DECK","TV","TAP","NEWS","NET","NANNY","NEEM","SONAR sensor","NAIL","SNOWSHOE","NUKE","KNIFE","NIB","MAZE","MAT","MINE","MUMMY","HAMMER","MAIL","MATCH","MUG","MOVIE","MOP","RICE","ROD","RAIN","RAM","RAR","RAIL", "RIDGE", "RAKE","ROOF","ROPE","LACE","LED","LANE", "LOOM", "LORRY","LILY","LEASH","LOCK","LEAF","LAB","JUICE","JET","GENIE","GEM","JAR","JAIL","JUDGE","JACK","CHEF","JEEP","GAS","KITE","GUN","CAM","CAR","COIL","CASH","CAKE","CUFF","CUP","VACE","VIDEO","FAN","FOAM","FIRE","FOIL","FISH","FOGG deodorant spray","FIFA","VIP pass","BUS","BED","BIN","PALM","BAR","BELL","POUCH","BIKE","BUFFET","PIPE"];
 
-const conditionImages: string[] = ["0-scratched.png", "1-tarred.png", "2-nailed.png", "3-muddied.png", "4-rusted.png", "5-labelled.png", "6-chopped.png", "7-greased.png", "8-firedup.png", "9-papered.png"];
-const conditionLabels: string[] = [", with a SCRATCHED surface revealing its inside ", ", covered with TAR in ", ", sealed using NAILS in ", ", in a MUDDIED ", ", RUSTED in ", ", LABELLED in ", ", CHOPPED to reveal its inside ", ", GREASED in ", ", emitting FLAMES in ", ", all PAPERED in "];
+const conditionImages: string[] = ["0-scratched.png", "1-tarred.png", "2-nailed.png", "3-muddied.png", "4-rusted.png", "5-labelled.png", "6-jagged.png", "7-greased.png", "8-firedup.png", "9-papered.png"];
+const conditionLabels: string[] = [", with a SCRATCHED surface revealing its inside ", ", covered with TAR in ", ", sealed using NAILS in ", ", in a MUDDIED ", ", RUSTED in ", ", LABELLED in ", ", JAGGED to dangerously expose its sharp corners & edges in", ", GREASED in ", ", emitting FLAMES in ", ", all PAPERED in "];
 
 // https://www.spycolor.com/color-index,g
 const colorCodes: string[] = ["#f0f0f1", "#00f5ff", "#000080", "magenta", "red", "#ccff00", "#4f0013", "#ffd700", "white", "black"];
 const colorLabels: string[] = ["SILVER color, somewhere ", "TURQUOISE color, somewhere ", "NAVY-BLUE color, somewhere ", "MAROON color, somewhere ", "RED color, somewhere ", "LEMON color, somewhere ", "CHOCOLATY color, somewhere ", "GOLD color, somewhere ", "WHITE color, somewhere ", "BLACK color, somewhere "];
 
-const locationImages: string[] = ["0-city.png", "1-desert.png", "2-north-pole.png", "3-mountain2.png", "4-river.png", "5-lake.png", "6-jungle.png", "7-coast.png", "8-valley.png", "9-party.png"];
+const locationImages: string[] = ["0-city.png", "1-desert.png", "2-north-pole.png", "3-mountain.png", "4-river.png", "5-lake.png", "6-jungle.png", "7-coast.png", "8-waterfall.png", "9-party.png"];
 const locationLabels: string[] = ["within CITY", "within a DESERT", "at the NORTH POLE", "within MOUNTAINS", "along side the RIVER", "by the LAKE", "in the JUNGLE", "at the COAST", "in the VALLEY", "at the PARTY DESTINATION"];
-const weatherImages: string[] = ["0-snowfall2.png", "1-dust.png", "2-normal2.png", "3-moist.png", "4-rain.png", "5-lightning.png", "6-chilling-air.png", "7-cloud .png", "8-fog.png", "9-pollution.png"];
+const weatherImages: string[] = ["0-snowfall.png", "1-dust.png", "2-normal2.png", "3-moist.png", "4-rain.png", "5-lightning.png", "6-chilling-air.png", "7-cloud.png", "8-fog.png", "9-pollution.png"];
 const weatherLabels: string[] = [" while SNOWFALL"," in DUSTY weather"," in NORMAL weather"," in MOIST and humid weather"," under RAIN"," in heavy thunder LIGTNING"," in CHILLING weather"," under CLEAR SKY"," in FOGGY weather conditions"," POLLUTED by heavy traffic"];
 const timeColorCodes: string[] = ["black", "#191D30", "#93ABB5", "#87ceeb", "#d7e8fd", "#D4FFF7", "#FEFFD4", "#d7e8fd", "#455270", "#2C3342"];
 const timeImages: string[] = ["0-12am-midnight.png", "1-3am-latenight.png", "2-6am-dawn.png", "3-8am-morning.png", "4-10am-sunny-morning.png", "5-12pm-noon.png", "6-3pm-noon.png", "7-6pm-evening.png", "8-8pm-late-evening.png", "9-10pm-moonlight-dinner.png"];
@@ -47,26 +47,27 @@ const SOUND_PLACE = 10000000; // 7th digit from right
 * 3. Setup ACTOR and its makeup to map next 4 digits
 */
 // Picture the actor (Next 2 digits)
-const ROLE_PLACE = 1000000000; // 9th digit from right
-const CHARACTER_PLACE = 100000000; // 8th digit from right
+const ROLE_PLACE = 100000000; // 9th digit from right
+const CHARACTER_PLACE = 1000000000; // 8th digit from right
 // Picture the actor's injury (Next 2 digits)
-const ORGAN_PLACE = 100000000000; // 11th digit from right
-const INJURY_PLACE = 10000000000; // 10th digit from right
+const ORGAN_PLACE = 10000000000; // 11th digit from right
+const INJURY_PLACE = 100000000000; // 12th digit from right
+const FEELINGS_PLACE = 1000000000000; // 13th digit from right
 
 /*
 * 4. Setup the PALACE to map next 2 digits
 */
 // Picture the memory palace and its stations (Next 2 digits)
-const LOCI_PLACE = 10000000000000; // 13th digit from right
-const STATION_PLACE = 1000000000000; // 12th digit from right
+const LOCI_PLACE = 10000000000000; // 14th digit from right
+const STATION_PLACE = 100000000000000; // 15th digit from right
 
 
-const CALAMITY_PLACE = 100000000000000;
+const CALAMITY_PLACE = 1000000000000000; // 16th digit from right
 
 /*
 * 5. Setup the PLANET to map next 2 digits
 */
-const PLANET_PLACE = 1000000000000000; // 14th digit from right
+const PLANET_PLACE = 10000000000000000; // 17th digit from right
 
 let getPegIndex = (n: number, decimalPlaceValue: number, numberOfDigitsToExtract: number) => {
     return ((n - n % decimalPlaceValue) / decimalPlaceValue) % [10, 100][numberOfDigitsToExtract - 1];
@@ -86,7 +87,8 @@ let numberToScene = (n: number) => {
     let actorRoleIdx = getPegIndex(n, ROLE_PLACE, 1);
     let actorCharacterIdx = getPegIndex(n, CHARACTER_PLACE, 1);
     let actorOrganIdx = getPegIndex(n, ORGAN_PLACE, 1);
-    let actorInjuryIdx = getPegIndex(n, INJURY_PLACE, 1)
+    let actorInjuryIdx = getPegIndex(n, INJURY_PLACE, 1);
+    let actorFeelingsIdx = getPegIndex(n, FEELINGS_PLACE, 1);
     let palaceLociIdx = getPegIndex(n, LOCI_PLACE, 1);
     let palaceStationIdx = getPegIndex(n, STATION_PLACE, 1);
     let calamityIdx = getPegIndex(n, CALAMITY_PLACE, 1);
@@ -99,13 +101,13 @@ let numberToScene = (n: number) => {
                 n: propObjectIdx,
                 label: propLabels[propObjectIdx],
                 image: propImages[propObjectIdx],
-                imagePath: "./images/objects/" + propImages[propObjectIdx]
+                imagePath: "./images/objects/"
             },
             condition: {
                 n: propConditionIdx,
                 label: conditionLabels[propConditionIdx],
                 image: conditionImages[propConditionIdx],
-                imagePath: "./images/object-conditions/" + conditionImages[propConditionIdx]
+                imagePath: "./images/object-conditions/"
             },
             color: {
                 n: propColorIdx,
@@ -118,20 +120,20 @@ let numberToScene = (n: number) => {
                 n: siteLocationIdx,
                 label: locationLabels[siteLocationIdx],
                 image: locationImages[siteLocationIdx],
-                imagePath: "./images/sites/clip-art/" + locationImages[siteLocationIdx]
+                imagePath: "./images/sites/"
             },
             weather: {
                 n: siteWeatherIdx,
                 label: weatherLabels[siteWeatherIdx],
                 image: weatherImages[siteWeatherIdx],
-                imagePath: "./images/weather/" + weatherImages[siteWeatherIdx]
+                imagePath: "./images/weather/"
             },
             time: {
                 n: siteTimeIdx,
                 label: timeLabels[siteTimeIdx],
                 clock: clockLabels[siteTimeIdx],
                 image: timeImages[siteTimeIdx],
-                imagePath: "./images/time/" + timeImages[siteTimeIdx],
+                imagePath: "./images/time/",
                 color: timeColorCodes[siteTimeIdx]
             },
             sound: {
@@ -151,6 +153,9 @@ let numberToScene = (n: number) => {
             },
             injury: {
                 
+            },
+            feelings: {
+                
             }
         },
         palace: {
@@ -165,7 +170,7 @@ let numberToScene = (n: number) => {
             n: calamityIdx,
             label: " [No calamity text set] ",
             image: calamityImages[calamityIdx],
-            imagePath: "./images/calamities/" + calamityImages[calamityIdx]
+            imagePath: "./images/calamities/"
         },
         planet: {
             
@@ -193,34 +198,40 @@ let renderLabels= (scene: any, labelId: string) => {
     if (label) label.innerHTML = "Picture a " + text;
 }
 
+let renderSite = (scene: any, suffix: string, path: string) => {
+
+    let site: any = document.getElementById("site" + suffix);
+    if (site) site.style.backgroundColor = scene.site.time.color;
+
+    let location: any = document.getElementById("location" + suffix);
+    if (location) location.src = scene.site.location.imagePath + path + scene.site.location.image;
+
+    let weather: any = document.getElementById("weather" + suffix);
+    if (weather) weather.src = scene.site.weather.imagePath + scene.site.weather.image;
+
+    let time: any = document.getElementById("time" + suffix);
+    if (time) time.src = scene.site.time.imagePath + scene.site.time.image;
+
+    let clock: any = document.getElementById("clock" + suffix);
+    if (clock) clock.innerHTML = scene.site.time.clock;
+}
+
 let renderScene = (scene: any) => {
     console.log("Rendering Scene %o", scene);
 
     let obj: any = document.getElementById("object");
-    if (obj) obj.src = scene.prop.object.imagePath;
+    if (obj) obj.src = scene.prop.object.imagePath + scene.prop.object.image;
 
     let condition: any = document.getElementById("condition");
     if (condition) {
-        condition.src = scene.prop.condition.imagePath;
+        condition.src = scene.prop.condition.imagePath + scene.prop.condition.image;
     }
 
     let color: any = document.getElementById("color");
     if (color) color.style.backgroundColor = scene.prop.color.color;
 
-    let siteContainer: any = document.getElementById("siteContainer");
-    if (siteContainer) siteContainer.style.backgroundColor = scene.site.time.color;
-
-    let location: any = document.getElementById("location");
-    if (location) location.src = scene.site.location.imagePath;
-
-    let weather: any = document.getElementById("weather");
-    if (weather) weather.src = scene.site.weather.imagePath;
-
-    let time: any = document.getElementById("time");
-    if (time) time.src = scene.site.time.imagePath;
-
-    let clock: any = document.getElementById("clock");
-    if (clock) clock.innerHTML = scene.site.time.clock;
+    renderSite(scene, "", "clip-art/");
+    renderSite(scene, "Real", "real/");
 
     let calamity: any = document.getElementById("calamity");
     if (calamity) calamity.src = scene.calamity.imagePath;
