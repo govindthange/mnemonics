71862dbe6ce303f29fc40d4a1606e8c57a06b1ad
Govind Thange <govindt@pune.winsoftech.com>
2020-06-05 19:10:07 +0530

    Modularize logic

--- src/index.ts
+++ src/index.ts
@@ -7,64 +7,34 @@ import { Scene } from "./pegs/Scene";
 //new DefaultPanel();
 
 let renderLabels = (txt: any, labelId: string) => {
-    console.log("Rendering label for Scene %o", txt);
     let label: any = document.getElementById(labelId);
     if (label) label.innerHTML = txt;
 }
 
-let processText = (t: any) => {
-    console.log(t);
-    let temp = Scene.encode(t);
-    renderLabels(temp, "encodedText");
+let processText = (text: any) => {
 
-    let s: Scene = new Scene();
-    let txt = "Imagine following items: <br/>";
-    let arr = temp.trim().split(" ");
-    for(let item of arr) {
-
-        if (item.trim().length == 0) {
-            continue;
+    let story: any = Scene.createStory(text);
+    let cnt = 1;
+    let narration = "Imagine following items: <br/>";
+    for(let plot of story.plot) {
+        if (plot.scene) {
+            narration += `<p>${cnt++}. A ${plot.scene} | <span class='number-span'>${plot.key}</span></p>`;
         }
-        
-        try {
-            if (item.length%2 != 0) {
-                item += item[item.length-1];
-            }
-            let n = parseInt(item);
-            s.update(n, true);
-            txt += "<b> [" + item + "]</b> A " + s;
-        } catch (error) {
-            txt +=  "<b> [" + item + "]</b>" + error.toString();
+        else {
+            narration +=  `<p><span class='number-err'>${plot.key}</span>${plot.error.toString()}</p>`;
         }
-       
-        txt += "<BR/>";
     }
-    renderLabels(txt, "label");
-}
 
-let processNumber = (n: any) => {
-    console.log(`Memory mapping ${n}!`);
-    let scene: Scene = new Scene();
-    scene.update(n, true);
-    renderLabels(scene, "label");
+    renderLabels(story.transcript, "transcript");
+    renderLabels(narration, "narration");
 }
 
 const textInput = document.querySelector('#textInput');
 const textInput$ = fromEvent(textInput, 'input')
-                .pipe(map((evt: any) => evt.target.value));
+                    .pipe(map((evt: any) => evt.target.value));
 
 textInput$.subscribe({
     next: t =>  processText(t),
-    error: err => console.log(`Oops... ${err}`),
-    complete: () => console.log(`Complete!`),
-});
-
-const numberInput = document.querySelector('input[type=number]');
-const numberInput$ = fromEvent(numberInput, 'input')
-                .pipe(map((evt: any) => evt.target.value));
-
- numberInput$.subscribe({
-    next: n =>  processNumber(n),
     error: err => console.log(`Oops... ${err}`),
     complete: () => console.log(`Complete!`),
 });
